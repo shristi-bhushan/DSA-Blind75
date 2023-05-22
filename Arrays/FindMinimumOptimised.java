4c1cb3f039d8d488d544960e2bc863451d468262
shristi-bhushan <shristibhushan2001@gmail.com>
2023-05-22 22:55:31 +0530

    solution added

--- Arrays/FindMinimumOptimised.java
+++ Arrays/FindMinimumOptimised.java
@@ -7,3 +7,21 @@
 
 // Space Complexity: 
 // O(1)
+
+class Solution {
+public static int findMinimum(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[right]) {
+                right = mid; // Minimum value is in the left half
+            } else {
+                left = mid + 1; // Minimum value is in the right half
+            }
+        }
+
+        return nums[left]; // Minimum value found
+    }
+}
