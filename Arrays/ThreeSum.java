b1c7da74319cdeb207f78520a3870f672f527ed0
shristi-bhushan <shristibhushan2001@gmail.com>
2023-05-23 23:52:44 +0530

    solution

---
+++ Arrays/ThreeSum.java
@@ -0,0 +1,32 @@
+Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1;
+                int right = nums.length - 1;
+                int target = -nums[i];
+
+                while (left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (sum == target) {
+                        List<Integer> triplet = new ArrayList<>();
+                        triplet.add(nums[i]);
+                        triplet.add(nums[left]);
+                        triplet.add(nums[right]);
+                        result.add(triplet);
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
